7479f762f1bf85eb852943ec163ebc38382ff2ea
seethroughthesnow <154474467+seethroughthesnow@users.noreply.github.com>
2023-12-20 19:23:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ def update_json_with_music(folder_path, output_json_path):
         "_shuffled": True
     }
 
-    # add the music files to the JSON
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.lower().endswith(('.mp3', '.wav', '.ogg')):  # Add more extensions if needed
                 full_path = os.path.join(root, file)
                 music_data["_ids"].append({"path": full_path, "type": 1})
 
-    # Write the updated JSON to a file
     with open(output_json_path, 'w') as json_file:
         json.dump(music_data, json_file, indent=2)
 
-folder_path = "D:\\Programs\\Steam\\steamapps\\common\\ULTRAKILL\\CyberGrind\\Music"
+# Dont forget to change your folder_path
+folder_path = "D:\\Programs\\Steam\\steamapps\\common\\ULTRAKILL\\CyberGrind\\Music" 
 output_json_path = "slot1.json"
 
 update_json_with_music(folder_path, output_json_path)
